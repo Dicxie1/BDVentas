cea8ac8bf679e3243e75ca204e496b0a9dede605
Dicxie Madrigal <hidannara@yahoo.es>
2021-02-28 23:36:37 -0600

    Se agrego las setencias ALTER y DROP DATABASE al archivo 01Database.sql

--- 01 - DDL/01Database.sql
+++ 01 - DDL/01Database.sql
@@ -15,3 +15,30 @@ CREATE DATABASE Ventas;
   ejemplo:
   CREATE SCHEMA Ventas
 */
+/*Crear dos base de datos llamados db1 , db2*/
+CREATE DATABASE db1;
+CREATE DATABASE db2;
+
+/*Mostrar base de datos creados con la setencias SHOW DATABASES*/
+SHOW DATABASES;
+
+/*Para modificar para modificar una base de datos se utiliza la setencias ALTER. 
+  A las base de datos unicamente se le puede modificar los COLLACTION que su funcion es definir
+  cómo debe comparar el texto y/o ordenarlo
+  Sintaxis:
+    ALTER DATABASE dbnombre opcions.
+*/
+ALTER DATABASE Ventas COLLATE utf8_bin;
+/*otros ejemplos de moficaciòn*/
+ALTER DATABASE db1 COLLATE ascii_bin; 
+ALTER DATABASE db2 COLLATE latin1_bin; 
+
+/*
+   Con la setencia DROP es utilizado para eliminar una base de datos 
+   Sintaxis:
+   DROP{DATABASE | SCHEMA} [IF EXISTS] dbnombre;
+   Ejemplo:
+
+*/
+DROP DATABASE db1;
+DROP DATABASE db2;
