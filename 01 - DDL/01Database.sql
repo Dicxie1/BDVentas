54688ec2f9b77646d5c3cf4a8ab6e2af7410a5b8
Dicxie Madrigal <hidannara@yahoo.es>
2021-02-28 22:40:41 -0600

    se modifico modifico el archivo 01Database.sql agregado las sentencias SQL para crea la tablas ventas

--- 01 - DDL/01Database.sql
+++ 01 - DDL/01Database.sql
@@ -1,4 +1,17 @@
 /*
- * SQL (Structure Query Lenguage ) es un lenguaje de base de datos que
+ * Para CREAR una base de datos se utiliza la clausula CREATE.
+ * Sintaxis:
+    CREATE {DATABASE | SCHEMA } [IF NOT EXISTS] dbnombre;
+
+    donde: 
+        DATABASE Y SCHEMA: son sinonimo y puede intercambiable entre ambas.
+        IF NOT EXISTS: opcional donde especifica la creaciòn de la base de datos ni no existe con el mismo nombres
+        dbnombre: hace referencias al nombre de la base de dato
  * 
+*/
+/*Creación de la base de datos con el nombre Ventas**/
+CREATE DATABASE Ventas;
+/*el anterior base de datos creado pudo haberce creado con la palabra SCHEMA
+  ejemplo:
+  CREATE SCHEMA Ventas
 */
