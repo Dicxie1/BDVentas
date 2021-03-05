d5546327825c166513e12656db6ddbe426988102
Dicxie Madrigal <dicxiemadrigal@gmail.com>
2021-03-05 14:11:24 -0600

    add a image form de scheme of the dababase BDventas

--- 02-DML/01insert.sql
+++ 02-DML/01insert.sql
@@ -1,3 +1,8 @@
+/*introducir registro en la tabla proveedor*/
+INSERT INTO proveedor VALUE(1,'DELL', 'Av. 201');
+/*definir el orden que se introduce el registro*/
+INSERT INTO proveedor(idproveedor, direccion, nombre) VALUE(2, 'Av. San Bolivar 21', 'Compact');
 /*insertar registros*/
-
+INSERT INTO producto value('P0001', 'Mouse', 20)
 /*insertar un registro a sierta columnas*/
+INSERT INTO producto() value('', '');
